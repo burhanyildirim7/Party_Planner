f53a9dbe9c1c3a4e4e462ae5214ecc9c10bf8e21
Ali Cam <ali.cam5510@gmail.com>
2022-02-21 11:09:41 +0300

    Genel olarak Test İçin prefablara küp atandı

--- Assets/Scripts/EkstraKodlar/Bina/Bina_Punch.cs
+++ Assets/Scripts/EkstraKodlar/Bina/Bina_Punch.cs
@@ -57,6 +57,15 @@ public class Bina_Punch : MonoBehaviour
             Destroy(olusanEsyalar[i]);
         }
         olusanEsyalar.Clear();
+
+
+        obje1Sirasi = 0;
+        obje2Sirasi = 0;
+        obje3Sirasi = 0;
+        obje4Sirasi = 0;
+        obje5Sirasi = 0;
+        obje1_TekSirasi = 0;
+        obje2_TekSirasi = 0;
     }
 
 
@@ -73,20 +82,23 @@ public class Bina_Punch : MonoBehaviour
 
 
 
+
+
+
     private void Obje1Yerlestir(string isim)
     {
         GameObject obje;
         if (isim == "Obje1Iyi(Clone)")
         {
             obje = Instantiate(obje1[0], obje1Konumlari[obje1Sirasi].transform.position, Quaternion.Euler(-Vector3.right * 90));
-            obje.transform.localScale = Vector3.one * 4;
+            olusanEsyalar.Add(obje);
 
             obje1Sirasi++;
         }
         else if (isim == "Obje1Kotu(Clone)")
         {
             obje = Instantiate(obje1[1], obje1Konumlari[obje1Sirasi].transform.position, Quaternion.Euler(-Vector3.right * 90));
-            obje.transform.localScale = Vector3.one * 4;
+            olusanEsyalar.Add(obje);
 
             obje1Sirasi++;
         }
@@ -94,15 +106,18 @@ public class Bina_Punch : MonoBehaviour
 
     private void Obje2Yerlestir(string isim)
     {
+        GameObject obje;
         if (isim == "Obje2Iyi(Clone)")
         {
-            Instantiate(obje2[0], obje2Konumlari[obje2Sirasi].transform.position, Quaternion.Euler(-Vector3.right * 90 + Vector3.forward * 180));
+            obje = Instantiate(obje2[0], obje2Konumlari[obje2Sirasi].transform.position, Quaternion.Euler(-Vector3.right * 90 + Vector3.forward * 180));
+            olusanEsyalar.Add(obje);
 
             obje2Sirasi++;
         }
         else if (isim == "Obje2Kotu(Clone)")
         {
-            Instantiate(obje2[1], obje2Konumlari[obje2Sirasi].transform.position, Quaternion.Euler(-Vector3.right * 90 + Vector3.forward * 180));
+            obje = Instantiate(obje2[1], obje2Konumlari[obje2Sirasi].transform.position, Quaternion.Euler(-Vector3.right * 90 + Vector3.forward * 180));
+            olusanEsyalar.Add(obje);
 
             obje2Sirasi++;
         }
@@ -110,15 +125,18 @@ public class Bina_Punch : MonoBehaviour
 
     private void Obje3Yerlestir(string isim)
     {
+        GameObject obje;
         if (isim == "Obje3Iyi(Clone)")
         {
-            Instantiate(obje3[0], obje3Konumlari[obje3Sirasi].transform.position, Quaternion.identity);
+            obje = Instantiate(obje3[0], obje3Konumlari[obje3Sirasi].transform.position, Quaternion.identity);
+            olusanEsyalar.Add(obje);
 
             obje3Sirasi++;
         }
         else if (isim == "Obje3Kotu(Clone)")
         {
-            Instantiate(obje3[1], obje3Konumlari[obje3Sirasi].transform.position, Quaternion.identity);
+            obje = Instantiate(obje3[1], obje3Konumlari[obje3Sirasi].transform.position, Quaternion.identity);
+            olusanEsyalar.Add(obje);
 
             obje3Sirasi++;
         }
@@ -126,15 +144,18 @@ public class Bina_Punch : MonoBehaviour
 
     private void Obje4Yerlestir(string isim)
     {
+        GameObject obje;
         if (isim == "Obje4Iyi(Clone)")
         {
-            Instantiate(obje4[0], obje4Konumlari[obje4Sirasi].transform.position, Quaternion.identity);
+            obje = Instantiate(obje4[0], obje4Konumlari[obje4Sirasi].transform.position, Quaternion.identity);
+            olusanEsyalar.Add(obje);
 
             obje4Sirasi++;
         }
         else if (isim == "Obje4Kotu(Clone)")
         {
-            Instantiate(obje4[1], obje4Konumlari[obje4Sirasi].transform.position, Quaternion.identity);
+            obje = Instantiate(obje4[1], obje4Konumlari[obje4Sirasi].transform.position, Quaternion.identity);
+            olusanEsyalar.Add(obje);
 
             obje4Sirasi++;
         }
@@ -142,15 +163,18 @@ public class Bina_Punch : MonoBehaviour
 
     private void Obje5Yerlestir(string isim)
     {
+        GameObject obje;
         if (isim == "Obje5Iyi(Clone)")
         {
-            Instantiate(obje5[0], obje5Konumlari[obje5Sirasi].transform.position, Quaternion.identity);
+            obje = Instantiate(obje5[0], obje5Konumlari[obje5Sirasi].transform.position, Quaternion.identity);
+            olusanEsyalar.Add(obje);
 
             obje5Sirasi++;
         }
         else if (isim == "Obje5Kotu(Clone)")
         {
-            Instantiate(obje5[1], obje5Konumlari[obje5Sirasi].transform.position, Quaternion.identity);
+            obje = Instantiate(obje5[1], obje5Konumlari[obje5Sirasi].transform.position, Quaternion.identity);
+            olusanEsyalar.Add(obje);
 
             obje5Sirasi++;
         }
@@ -158,15 +182,18 @@ public class Bina_Punch : MonoBehaviour
 
     private void Obje1TekYerlestir(string isim)
     {
+        GameObject obje;
         if (isim == "Obje1TekIyi(Clone)")
         {
-            Instantiate(obje1_Tek[0], obje1Tek_Konumlari[obje5Sirasi].transform.position, Quaternion.identity);
+            obje = Instantiate(obje1_Tek[0], obje1Tek_Konumlari[obje5Sirasi].transform.position, Quaternion.identity);
+            olusanEsyalar.Add(obje);
 
             obje1_TekSirasi++;
         }
         else if (isim == "Obje1TekKotu(Clone)")
         {
-            Instantiate(obje1_Tek[1], obje1Tek_Konumlari[obje5Sirasi].transform.position, Quaternion.identity);
+            obje = Instantiate(obje1_Tek[1], obje1Tek_Konumlari[obje5Sirasi].transform.position, Quaternion.identity);
+            olusanEsyalar.Add(obje);
 
             obje1_TekSirasi++;
         }
@@ -174,15 +201,18 @@ public class Bina_Punch : MonoBehaviour
 
     private void Obje2TekYerlestir(string isim)
     {
+        GameObject obje;
         if (isim == "Obje2TekIyi(Clone)")
         {
-            Instantiate(obje2_Tek[0], obje2Tek_Konumlari[obje1_TekSirasi].transform.position, Quaternion.identity);
+            obje = Instantiate(obje2_Tek[0], obje2Tek_Konumlari[obje1_TekSirasi].transform.position, Quaternion.identity);
+            olusanEsyalar.Add(obje);
 
             obje2_TekSirasi++;
         }
         else if (isim == "Obje2TekKotu(Clone)")
         {
-            Instantiate(obje2_Tek[1], obje2Tek_Konumlari[obje2_TekSirasi].transform.position, Quaternion.identity);
+            obje = Instantiate(obje2_Tek[1], obje2Tek_Konumlari[obje2_TekSirasi].transform.position, Quaternion.identity);
+            olusanEsyalar.Add(obje);
 
             obje2_TekSirasi++;
         }
